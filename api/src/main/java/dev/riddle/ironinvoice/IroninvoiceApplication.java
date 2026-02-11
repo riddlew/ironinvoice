dd48bf48a0eda0c91e5f5f5365e091838fc8cdb4
W. Riddle <spcgtswe@gmail.com>
2026-02-11 05:09:42 -0500

    Initial commit

---
+++ api/src/main/java/dev/riddle/ironinvoice/IroninvoiceApplication.java
@@ -0,0 +1,13 @@
+package dev.riddle.ironinvoice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IroninvoiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IroninvoiceApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/dev/riddle/ironinvoice/IroninvoiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.riddle.ironinvoice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IroninvoiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
