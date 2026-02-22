07dfd0931ba2868153b466b30d0b2cd18634c250
W. Riddle <spcgtswe@gmail.com>
2026-02-22 18:31:14 -0500

    log: add error logging to `FallbackExceptionHandler` for improved debugging

--- api/src/main/java/dev/riddle/ironinvoice/api/error/handlers/FallbackExceptionHandler.java
+++ api/src/main/java/dev/riddle/ironinvoice/api/error/handlers/FallbackExceptionHandler.java
@@ -2,6 +2,8 @@ package dev.riddle.ironinvoice.api.error.handlers;
 
 import dev.riddle.ironinvoice.api.error.ApiError;
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.Instant;
 
+@Slf4j
 @RestControllerAdvice
 @Order()
 public class FallbackExceptionHandler {
@@ -23,6 +26,8 @@ public class FallbackExceptionHandler {
 			request.getRequestURI(),
 			Instant.now()
 		);
+
+		log.error("Internal server error", ex);
 
 		return ResponseEntity
 			.status(HttpStatus.INTERNAL_SERVER_ERROR)
