5e355909939652669b12309d1824b42d52da625e
W. Riddle <spcgtswe@gmail.com>
2026-02-22 11:29:05 -0500

    feat: add polymorphic type handling to `MappingSource` with `JsonTypeInfo`
    - Introduced `@JsonTypeInfo` and `@JsonSubTypes` annotations to enable polymorphic deserialization of `MappingSource`.

--- api/src/main/java/dev/riddle/ironinvoice/api/features/mappings/domain/mapping_config/sources/MappingSource.java
+++ api/src/main/java/dev/riddle/ironinvoice/api/features/mappings/domain/mapping_config/sources/MappingSource.java
@@ -1,5 +1,16 @@
 package dev.riddle.ironinvoice.api.features.mappings.domain.mapping_config.sources;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "source")
+@JsonSubTypes({
+	@JsonSubTypes.Type(name = "COLUMN", value = ColumnSource.class),
+	@JsonSubTypes.Type(name = "INDEX", value = IndexSource.class),
+	@JsonSubTypes.Type(name = "CONST", value = ConstSource.class),
+	@JsonSubTypes.Type(name = "NOW", value = NowSource.class),
+	@JsonSubTypes.Type(name = "EXPR", value = ExprSource.class)
+})
 public sealed interface MappingSource permits
 	ColumnSource, IndexSource, ConstSource, NowSource, ExprSource {
 }
